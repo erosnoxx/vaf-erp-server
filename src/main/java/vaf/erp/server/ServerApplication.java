5a2b45ace1412a6677fe0383746f3983d23e25cb
Eros Vieira <eros.vieira@evertecinc.com>
2026-02-28 17:24:56 -0300

    initialize project with Maven setup and Spring Boot application

---
+++ src/main/java/vaf/erp/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package vaf.erp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vaf/erp/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vaf.erp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
